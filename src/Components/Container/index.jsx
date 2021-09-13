9cfb6bfc3e82f2d0bac548f4735ee830bc70fae0
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2021-09-13 16:19:47 -0300

    css refatorado

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -14,6 +14,14 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+
+
+  @media (max-width: 800px) {
+    .conteudo {
+      flex-direction: column;
+    }
+  }
+
 `
 
 export default () => {
