77def0364af54abfa871262e853c501d03a3eafe
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2021-09-13 23:10:32 -0300

    implementado extrato

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle"
 
+
 function App() {
   return (
     <>
